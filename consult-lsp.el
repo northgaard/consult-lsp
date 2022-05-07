6074a98209c43bab31b2354fb8ced4d7e36cec33
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2022-05-07 14:26:16 +0530

    Respect `lsp-use-plists` (#30)
    (cherry picked from commit 19606a03cf854e1b0930c4526ed92c4560dccdc2)

--- consult-lsp.el
+++ consult-lsp.el
@@ -304,8 +304,8 @@ CURRENT-WORKSPACE? has the same meaning as in `lsp-diagnostics'."
          (all-workspaces? arg)
          (ws (or (and all-workspaces? (-uniq (-flatten (ht-values (lsp-session-folder->servers (lsp-session))))))
                  (lsp-workspaces)
-                 (gethash (lsp-workspace-root default-directory)
-                          (lsp-session-folder->servers (lsp-session))))))
+                 (lsp-get (lsp-session-folder->servers (lsp-session))
+                          (lsp-workspace-root default-directory)))))
     (unless ws
       (user-error "There is no active workspace !"))
     (consult--read
